f9d7173ee76064a7e5eae30d5ea51188db153f1f
KonstantinMosin <msnknstntn@gmail.com>
2022-12-09 01:53:48 +0300

    Update server and socket: add async prymityw

--- Server/server.h
+++ Server/server.h
@@ -1,8 +1,14 @@
 #ifndef SERVER_H
 #define SERVER_H
 
-#include <QObject>
 #include <QTcpServer>
+#include <QTcpSocket>
+#include <QDateTime>
+#include <QMultiMap>
+#include <QObject>
+#include <QTimer>
+#include <QMap>
+
 #include "socket.h"
 
 class Server : public QTcpServer {
@@ -11,8 +17,19 @@ public:
     explicit Server(QObject *parent = nullptr);
     void start();
 
+protected:
+    void incomingConnection(qintptr descriptor) Q_DECL_OVERRIDE;
+
+protected slots:
+    void update(qintptr descriptor);
+
 private slots:
-    void newConnection();
+    void idle();
+
+private:
+    QMap<qintptr, Socket *> raw_sockets;
+    QMultiMap<qint32, Socket *> avalaible_sockets;
+    QTimer * timer;
 };
 
 #endif // SERVER_H
--- Server/socket.cpp
+++ Server/socket.cpp
@@ -1,12 +1,19 @@
 #include "socket.h"
 
-Socket::Socket(QTcpSocket * socket) : socket_(socket) {
-
+Socket::Socket(QObject * parent, qintptr descriptor) : QObject{parent}, socket_(new QTcpSocket()), connected_time(QDateTime::currentDateTime().toTime_t()) {
+    socket_->setSocketDescriptor(descriptor);
     connect(socket_, SIGNAL(connected()), this, SLOT(connected()));
     connect(socket_, SIGNAL(disconnected()), this, SLOT(disconnected()));
     connect(socket_, SIGNAL(readyRead()), this, SLOT(readyRead()));
+    connect(this, SIGNAL(processed(qintptr)), parent, SLOT(update(qintptr)));
+//    socket_->socketDescriptor();
 
     qDebug() << "client connected";
+}
+
+qint32 Socket::time() {
+    qint32 delta = QDateTime::currentDateTime().toTime_t() - connected_time;
+    return delta >= delay ? 0 : delay - delta;
 }
 
 void Socket::write(const char *msg) {
@@ -14,13 +21,21 @@ void Socket::write(const char *msg) {
 }
 
 void Socket::connected() {
-    qDebug() << "connected event";
+    /*
+        connected event
+    */
 }
 
 void Socket::disconnected() {
-    qDebug() << "disconnected event";
+    /*
+        disconnected event
+    */
+    socket_->deleteLater();
 }
 
 void Socket::readyRead() {
-    qDebug() << socket_->readAll();
+    QByteArray buffer = socket_->readAll();
+    QString data(buffer);
+    delay = data.split(" ")[0].toUInt();
+    emit processed(socket_->socketDescriptor());
 }
