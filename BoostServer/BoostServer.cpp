dd8b9be0cae6f3e2592001a62129e676f222e847
sanka-eu <sanyadimka@gmail.com>
2022-12-08 10:50:07 +0300

    Add echo-BoostServer

---
+++ BoostServer/BoostServer.cpp
@@ -0,0 +1,130 @@
+﻿// ConsoleApplication1.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
+//
+// async_tcp_echo_server.cpp
+// ~~~~~~~~~~~~~~~~~~~~~~~~~
+//
+// Copyright (c) 2003-2022 Christopher M. Kohlhoff (chris at kohlhoff dot com)
+//
+// Distributed under the Boost Software License, Version 1.0. (See accompanying
+// file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
+//
+
+#include <boost/asio.hpp>
+
+#include <string> // подключаем строки
+#include <fstream> // подключаем файлы
+#include <memory>
+#include <utility>
+#include <cstdlib>
+#include <iostream>
+
+using boost::asio::ip::tcp;
+
+class session
+    : public std::enable_shared_from_this<session>
+{
+public:
+    session(tcp::socket socket)
+        : socket_(std::move(socket))
+    {
+    }
+
+    void start()
+    {
+        do_read();
+    }
+
+private:
+    void do_read()
+    {
+        auto self(shared_from_this());
+        socket_.async_read_some(boost::asio::buffer(data_, max_length),
+            [this, self](boost::system::error_code ec, std::size_t length)
+            {
+                if (!ec)
+                {
+                    do_write(length);
+                }
+            });
+    }
+
+    void do_write(std::size_t length)
+    {
+        auto self(shared_from_this());
+        boost::asio::async_write(socket_, boost::asio::buffer(data_, length),
+            [this, self](boost::system::error_code ec, std::size_t /*length*/)
+            {
+                if (!ec)
+                {
+                    do_read();
+                }
+            });
+    }
+
+    tcp::socket socket_;
+    enum { max_length = 1024 };
+    char data_[max_length];
+};
+
+class server
+{
+public:
+    server(boost::asio::io_context& io_context, short port)
+        : acceptor_(io_context, tcp::endpoint(tcp::v4(), port))
+    {
+        do_accept();
+    }
+
+private:
+    void do_accept()
+    {
+        acceptor_.async_accept(
+            [this](boost::system::error_code ec, tcp::socket socket)
+            {
+                if (!ec)
+                {
+                    std::make_shared<session>(std::move(socket))->start();
+                }
+
+        do_accept();
+            });
+    }
+
+    tcp::acceptor acceptor_;
+};
+
+int main(int argc, char* argv[])
+{
+    try
+    {
+        std::ifstream file("C:\\Users\\Lenovo\\Desktop\\НИЦ\ РАБОТА\\C++\\ConsoleApplication1\\ConsoleApplication1\\port.dat");
+        std::string port;
+        getline(file, port);
+        file.close();
+
+        boost::asio::io_context io_context;
+
+        server s(io_context, std::atoi(port.c_str()));
+
+        io_context.run();
+    }
+    catch (std::exception& e)
+    {
+        std::cerr << "Exception: " << e.what() << "\n";
+    }
+
+    return 0;
+}
+
+// Запуск программы: CTRL+F5 или меню "Отладка" > "Запуск без отладки"
+// Отладка программы: F5 или меню "Отладка" > "Запустить отладку"
+
+// Советы по началу работы 
+//   1. В окне обозревателя решений можно добавлять файлы и управлять ими.
+//   2. В окне Team Explorer можно подключиться к системе управления версиями.
+//   3. В окне "Выходные данные" можно просматривать выходные данные сборки и другие сообщения.
+//   4. В окне "Список ошибок" можно просматривать ошибки.
+//   5. Последовательно выберите пункты меню "Проект" > "Добавить новый элемент", чтобы создать файлы кода, или "Проект" > "Добавить существующий элемент", чтобы добавить в проект существующие файлы кода.
+//   6. Чтобы снова открыть этот проект позже, выберите пункты меню "Файл" > "Открыть" > "Проект" и выберите SLN-файл.
