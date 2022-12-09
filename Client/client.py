5ca1c97290c16bf48eb414695d8484bb6e883800
KonstantinMosin <msnknstntn@gmail.com>
2022-12-09 16:20:04 +0300

    Update client: add compiled protobuf file

--- Client/client.py
+++ Client/client.py
@@ -1,11 +1,15 @@
 import socket
+import protocol_pb2
+
+message = protocol_pb2.WrapperMessage()
+message.request_for_slow_response.time_in_seconds_to_sleep = 5
 
 HOST = "127.0.0.1"
 PORT = 8000
 
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
     s.connect((HOST, PORT))
-    s.sendall(b"Greetings from server")
+    s.sendall(message.SerializeToString())
     data = s.recv(1024)
 
 print(f"Received {data!r}")
