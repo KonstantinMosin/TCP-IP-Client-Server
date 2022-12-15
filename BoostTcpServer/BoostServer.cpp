234410c3d55f97afe6bf9aefd9c822aa16c6a0bc
sanka-eu <sanyadimka@gmail.com>
2022-12-15 11:37:17 +0300

    addProtoMsg

--- BoostTcpServer/BoostServer.cpp
+++ BoostTcpServer/BoostServer.cpp
@@ -7,6 +7,8 @@
 #include <cstdlib>
 #include <iostream>
 
+#include "protobuf.pb.h"
+
 using boost::asio::ip::tcp;
 
 class session
@@ -27,12 +29,19 @@ private:
     void do_read()
     {
         auto self(shared_from_this());
-        socket_.async_read_some(boost::asio::buffer(data_, max_length),
+        //выделение памяти под сообщение
+        msg = new TestTask::Messages::WrapperMessage();
+        socket_.async_read_some(boost::asio::buffer(msg, max_length),
             [this, self](boost::system::error_code ec, std::size_t length)
             {
                 if (!ec)
                 {
                     do_write(length);
+                }
+                else
+                {
+                    //освобождение памяти в случае неудачи
+                    delete msg;
                 }
             });
     }
@@ -40,11 +49,13 @@ private:
     void do_write(std::size_t length)
     {
         auto self(shared_from_this());
-        boost::asio::async_write(socket_, boost::asio::buffer(data_, length),
+        boost::asio::async_write(socket_, boost::asio::buffer(msg, length),
             [this, self](boost::system::error_code ec, std::size_t /*length*/)
             {
                 if (!ec)
                 {
+                    //освобождение памяти после ответа клиенту
+                    delete msg;
                     do_read();
                 }
             });
@@ -52,7 +63,8 @@ private:
 
     tcp::socket socket_;
     enum { max_length = 1024 };
-    char data_[max_length];
+    //объявление сообщения как части сессии
+    TestTask::Messages::WrapperMessage* msg = nullptr;
 };
 
 class server
