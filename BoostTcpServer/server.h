ab8100b28757aa1f54d645f417b6b491dfa66377
sanka-eu <sanyadimka@gmail.com>
2022-12-16 12:02:36 +0300

    time_delay

--- BoostTcpServer/server.h
+++ BoostTcpServer/server.h
@@ -3,6 +3,9 @@
 #include "protobuf.pb.h"
 
 #include <boost/asio.hpp>
+//для асинхронной задержки
+#include <chrono>
+#include <thread>
 
 #include <memory>
 #include <utility>
@@ -12,14 +15,18 @@
 
 using boost::asio::ip::tcp;
 
+using namespace std::this_thread;     // sleep_for, sleep_until
+using namespace std::chrono;
+using std::chrono::system_clock;
+
 class Session
     : public std::enable_shared_from_this<Session>
 {
 public:
     Session(tcp::socket socket);
-
+    ~Session();
     void start();
-
+    void time_delay(TestTask::Messages::WrapperMessage* msg);
 private:
     void do_read();
 
