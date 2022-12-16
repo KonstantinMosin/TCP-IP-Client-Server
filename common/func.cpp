3416d28cbf77a37d52a3dc683850ddc3b146cf89
KonstantinMosin <msnknstntn@gmail.com>
2022-12-16 15:34:05 +0300

    Update common

--- common/func.cpp
+++ common/func.cpp
@@ -1,29 +1,32 @@
 #include "func.h"
 
 string convert_lu_to_string(size_t value) {
-    char buffer[4];
-    sprintf(buffer, "%04lu", value);
+    char buffer[5];
+    snprintf(buffer, 5, "%04lu", value);
     return string(buffer);
 }
 
-string fast_response() {
+string get_time() {
     using namespace std::chrono;
-    
-    string sample = "YYYYMMDDThhmmss";
-    
-    milliseconds ms = duration_cast<milliseconds>(system_clock::now().time_since_epoch());
-    
-    char buffer[sizeof(sample)];
-    time_t now = ms.count() / 1000;
-    strftime(buffer, sizeof(buffer), "%Y%m%dT%H%M%S", gmtime(&now));
-    
-    string response = string(buffer) + "." + std::to_string(ms.count() % 1000);
 
+    int64_t ms = duration_cast<milliseconds>(system_clock::now().time_since_epoch()).count();
+
+    char buffer[sizeof("YYYYMMDDThhmmss")];
+    time_t now = ms / 1000;
+    strftime(buffer, sizeof(buffer), "%Y%m%dT%H%M%S", gmtime(&now));
+
+    char converted_ms[4];
+    snprintf(converted_ms, 4, "%03ld", ms % 1000);
+
+    return string(buffer) + "." + string(converted_ms);
+}
+
+string fast_response() {
+    string response;
     WrapperMessage * message = new WrapperMessage();
-    message->mutable_fast_response()->set_current_date_time(response.c_str());
+    message->mutable_fast_response()->set_current_date_time(get_time().c_str());
     message->SerializeToString(&response);
     delete message;
-
     return convert_lu_to_string(response.size()) + response;
 }
 
