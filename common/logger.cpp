ec4b9e9763ff6c56e0fc22b4a118d1b235cff9cf
KonstantinMosin <msnknstntn@gmail.com>
2022-12-23 16:09:12 +0300

    Update logger

--- common/logger.cpp
+++ common/logger.cpp
@@ -1,5 +1,28 @@
 #include "logger.h"
 
-class Logger {
-    ;
-};
+Logger::Logger(string path, Level level) : f(path, std::ios::app), level(level) {
+    if (!f.is_open()) {
+        // ?
+    }
+    interpretation[Level::DEBUG] = "DEBUG";
+    interpretation[Level::INFO] = "INFO";
+    interpretation[Level::WARNING] = "WARNING";
+    interpretation[Level::ERROR] = "ERROR";
+    interpretation[Level::CRITICAL] = "CRITICAL";
+}
+
+Logger::~Logger() {
+    if (f.is_open()) {
+        f.close();
+    }
+}
+
+void Logger::setLevel(Level level) {
+    this->level = level;
+}
+
+void Logger::write(string message, Level level) {
+    if (f.is_open() && this->level <= level) {
+        f << interpretation[level] << ": " << message;
+    }
+}
